10d688fb72209c9249bdbd7210d091b2dc17e94b
Joyce Wu <jwu26@stuy.edu>
2017-09-15 13:18:24 -0400

    algorithms incomplete

--- occupations.py
+++ occupations.py
@@ -8,29 +8,33 @@ K03 -- StI/O: Divine your Destiny!
 
 import csv
 import random
+from decimal import Decimal
 
 dictionary = {} #creates blank dictionary 
 
 with open('occupations.csv', 'r') as csv_file: #opens csv file and reads it
     csv_reader = csv.reader(csv_file) 
-
     next(csv_reader) #skips headings
     for line in csv_reader: #goes through line by line
-       occupation = line[0] #takes occupation from each line
-       percent = line[1] #takes percent from eachline
-       dictionary[occupation]= percent #pairs occupation with its respective percent
+        if not (line[0] == "Total"): #does not include total in dictionary
+            occupation = line[0] #takes occupation from each line
+            percent = line[1] #takes percent from eachline
+            dictionary[occupation]= percent #pairs occupation with its respective percent
        
 
-print dictionary
+#print dictionary
 
 def rand_occupation():
     randnum = random.randint(0,100) #picks a number from 0-100
-    for percent in dictionary:  #goes through each percent in the dictionary
-        if (randnum > percent): #if number is greater than percent 
+    #print randnum
+    for occupation in dictionary:  #goes through each percent in the dictionary
+        val = dictionary.get(occupation)
+        if (randnum < Decimal(val)): #if number is greater than percent 
+            #print 10 * Decimal(val)
             print(occupation) #print the occupation
             break #allows only for one occupation to be printed
 
             
       
-
-rand_occupation()
+for x in range(100):
+    rand_occupation()
